e989c169562d8b4cd283523ef1fdcb7b581f5ab4
Daniel Barker <djbarker@users.noreply.github.com>
2014-01-06 18:03:43 +0000

    Primes
    Improved the implementation using tail-recursion. It is somewhat quicker
    than before but more importantly it no longer causes
    "OutOfMemoryException" for large N.

--- src/primes.fs
+++ src/primes.fs
@@ -1,22 +1,32 @@
 let nmax = 50000
 
-// very inefficient implementation of sieve of Eratosthenes
-let remove_multiples x m = 
-    x |> List.filter( fun i -> i%m<>0 )
+// improved implementation of sieve of Eratosthenes
+let sieve N = 
 
-let rec sieve x = 
-    match x with
-    | head :: tail -> 
-        if head<2 then
-            sieve tail
-        else
-            head :: sieve (remove_multiples tail head)
-    | [] -> []
+    let remove_multiples x m = 
+        x |> List.filter( fun i -> i%m<>0 )
 
-let primes = ((seq {1..nmax}) |> List.ofSeq |> sieve)
+    let rec sieve_impl x out = 
+        match x with
+        | head :: tail -> sieve_impl (remove_multiples tail head) (head::out)
+        | [] -> out
+
+    (sieve_impl (seq {2..nmax} |> List.ofSeq) []) |> List.rev
+
+(*
+    Returns the execution time of 'func' applied to 'arg1' in milliseconds and
+    the result in a pair. For functions with more than one argument partial
+    application can be used, e.g. (duration f x) y.
+ *)
+let duration func arg1 = 
+    let T = new System.Diagnostics.Stopwatch ()
+    T.Start ()
+    (T.ElapsedMilliseconds, func arg1)
+
+// calculate and store the primes
+let time, primes = duration sieve nmax
 
 primes |> List.iter( fun i -> printf "%d\t" i )
 
 printfn ""
-
-
+printfn "Duration: %d ms" time
