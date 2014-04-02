4055e75bc270534a4957dba0068557ea23f2b1ca
Daniel Barker <djbarker@users.noreply.github.com>
2014-04-02 12:15:13 +0100

    Program for checking if a string contains properly matched and nested brackets.

---
+++ src/bracket_check.fs
@@ -0,0 +1,70 @@
+﻿
+let valid_example = "([<{}>()])"
+let invalid_example = "([)]"
+
+exception ListException
+
+(*
+ * List operations
+ *)
+let front x = 
+    match x with
+    | [head] -> head
+    | head :: tail -> head
+    | _ -> raise ListException
+
+let pop x =
+    match x with
+    | [tail] -> []
+    | head :: tail -> tail
+    | _ -> raise ListException
+
+(*
+ * Bracket types
+ *) 
+type Brackets = 
+    | Paren  = 0
+    | Curley = 1
+    | Square = 2
+    | Angle  = 3
+
+(*
+ * Checks if the brackets are properly matched and nested in a string.
+ *)
+let is_valid str =
+    let stack = ref [] // mutable ref - so it can be captured by closure
+
+    let handle_bracket open_char close_char token char =
+        if char=open_char then
+            stack := token :: !stack
+        elif char=close_char then
+            if (front !stack)=token then
+                stack := pop !stack
+            else
+                raise ListException
+        char
+
+    try
+        str |> Seq.iter (fun char -> 
+                char 
+                |> handle_bracket '(' ')' Brackets.Paren
+                |> handle_bracket '{' '}' Brackets.Curley
+                |> handle_bracket '[' ']' Brackets.Square
+                |> handle_bracket '<' '>' Brackets.Angle
+                |> ignore
+            )
+
+        if List.length(!stack)=0 then
+            true
+        else
+            false
+    with
+    | ListException -> false
+
+// sanity checks
+assert (is_valid valid_example)
+assert not (is_valid invalid_example)
+    
+// an example
+let example = "template<> double test<double>(std::vector<int[3]>& x, std::function<int(int[3])>& y);"
+printfn "%A" (is_valid example)
